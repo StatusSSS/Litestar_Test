a7ec2e8fbb094548d52ee0831c1feac8e2855a34
Status <adm.bulldog@gmail.com>
2025-05-11 16:41:13 +0300

    update poetry

--- app/settings.py
+++ app/settings.py
@@ -1,26 +1,24 @@
-from pathlib import Path
 from functools import lru_cache
+from pathlib import Path
+
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent  # /app/..
 
-
 class Settings(BaseSettings):
+    debug: bool = Field(False, env="DEBUG")
 
-    debug: bool = Field(default=False, env="DEBUG")
-
-    database_url: Field(
-        default="postgresql+asyncpg://postgres:postgres@db:5432/postgres",
+    database_url: str = Field(
+        "postgresql+asyncpg://postgres:postgres@db:5432/postgres",
         env="DATABASE_URL",
     )
 
-    model_config = {
-        "env_file": ".env",
-        "env_file_encoding": "utf-8",
-        "case_sensitive": False,
-    }
-
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        case_sensitive=False,
+    )
 
 @lru_cache
 def get_settings() -> Settings:
